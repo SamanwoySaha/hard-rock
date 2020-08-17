8a399153ca0a6b5863a48a2d18167d6d2a5f3961
SamanwoySaha <samanwoy.kuet@gmail.com>
2020-08-17 18:39:00 +0600

    scrollbar issue fixed

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const searchInput = document.getElementById('search-term');
 const searchBtn = document.getElementById('search-btn');
 const searchResult = document.getElementById('search-result');
+const searchError = document.getElementById('search-error');
+
+const songInfo = document.getElementById('song-info');
 const songTitle = document.getElementById('song-title');
 const songLyric = document.getElementById('song-lyric');
 
@@ -10,12 +13,11 @@ searchBtn.addEventListener('click', showSearchResults);
 function showSearchResults() {
     let searchTerm = searchInput.value;
 
-    if(searchTerm === '' || searchTerm === null || typeof(searchTerm) !== 'string'){
+    if(searchTerm === '' || searchTerm === null){
         alert('Please enter your artist song name');
     }
     else {
-        getSearchResult(); 
-        searchTerm.value = '';       
+        getSearchResult();       
     }
 }
 
@@ -23,40 +25,46 @@ async function getSearchResult() {
     try {
         const response = await fetch(`https://api.lyrics.ovh/suggest/${searchInput.value}`);
         const result = await response.json();
-        console.log(result.data.length);
+        console.log(result.data[0]);
 
         if(result.data.length !== 0){
             const singleResult = result.data.slice(0, 10).map(song => `
                 <div class="single-result row align-items-center my-3 p-3">
                     <div class="col-md-9">
-                        <h3 class="lyrics-name">${song.title}</h3>
-                        <p class="author lead">Album by <span>${song.artist.name}</span></p>
+                        <img src="${song.album.cover}" class="img-fluid float-left mr-3" alt="cover-img">
+                        <h4 class="mb-3">
+                            <span class="badge badge-pill badge-info">Album</span>
+                            ${song.title}
+                        </h4>
+                        <p>
+                            <span class="badge badge-pill badge-light mb-3">Artist</span>
+                            <span>${song.artist.name}</span> 
+                            <br>Duration: ${Math.floor(song.duration/60)}m ${song.duration%60}s
+                        </p>
                     </div>
                     <div class="col-md-3 text-md-right text-center">
-                        <button data-title="${song.title}" data-artist="${song.artist.name}" onClick="showLyrics()" class="btn btn-success">Get Lyrics</button>
+                        <button data-title="${song.title}" data-artist="${song.artist.name}" onClick="getLyrics()" class="btn btn-success">Get Lyrics</button>
                     </div>
                 </div>  
             `).join('');
 
             searchResult.innerHTML = singleResult;
-            document.querySelector('.single-lyrics').style.display = "none";
+            songInfo.style.display = "none";
         }  
         else {
-            songTitle.innerText = 'No Search Result Found';
-            document.querySelector('.single-lyrics').style.display = "none";
-
+            searchError.innerText = 'No Search Result Found';
+            songInfo.style.display = "none"; 
         }
     }
     catch {
-        songTitle.innerText = 'No Search Result Found';
-        document.querySelector('.single-lyrics').style.display = "none";
-        document.getElementById('search-error').innerText = "No Search Results Found";
+        searchError.innerText = "No Search Results Found";
+        songInfo.style.display = "none";
     }
 }
 
 
 // get lyrics button event handler function
-async function showLyrics() {
+async function getLyrics() {
     const artist = event.target.dataset.artist;
     const title = event.target.dataset.title;
     songTitle.innerText = title;
@@ -65,11 +73,11 @@ async function showLyrics() {
         const response = await fetch(`https://api.lyrics.ovh/v1/'${artist}/${title}`);
         const data = await response.json();    
 
-        songLyric.innerHTML = data.lyrics.replace(new RegExp(",","g"), "<br>");
-        document.querySelector('.single-lyrics').style.display = "block";
+        songInfo.style.display = "block";
+        songLyric.innerHTML = data.lyrics.replace(/,/g, "\n");
     }
     catch {
+        songInfo.style.display = "block";
         songLyric.innerText = 'No Lyrics Found';
-        document.querySelector('.single-lyrics').style.display = "block";
     }
 }
